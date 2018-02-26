841db2be0e5e371efecf3ac6ca93fc3c9dcf0c23
Uwe Schaefer <uwe@codesmell.de>
2018-02-26 22:30:01 +0100

    Revert "uups, added a last min sleep"
    This reverts commit e7211412ba751d1da8a7c62bcbfed76f2437f352.

--- src/test/java/com/mercateo/edu/foobank/FooBank6Tests.java
+++ src/test/java/com/mercateo/edu/foobank/FooBank6Tests.java
@@ -51,11 +51,6 @@ public class FooBank6Tests {
 
         assertEquals(70, uwe.getBalance());
         assertEquals(30, andreas.getBalance());
-        
-        try {
-			Thread.sleep(200);
-		} catch (InterruptedException e) {
-		}
 
         verify(notification, Mockito.times(1)).sendEmailTo(andreasId, 30);
         verifyNoMoreInteractions(notification);
